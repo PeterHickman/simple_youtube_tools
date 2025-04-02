9625ac669729b2d9104afb4f4fc138716b3b0e0a
Peter Hickman <peterhickman386@googlemail.com>
2025-04-02 23:32:26 +0100

    simple works too

--- install.sh
+++ install.sh
@@ -1,21 +1,9 @@
 #!/bin/sh
 
-if ! [ "$(id -u)" = 0 ]; then
-  echo 'You must be root to do this.' 1>&2
-  exit 1
-fi
-
-##
-# When installing we would install as root, however on
-# BSD based systems (such as OSX) this is not a valid
-# group / user for install so we use the numeric value
-##
-ROOT="0"
-
 TARGET="/usr/local/bin/"
 
 for FILE in yt*
 do
   echo "Installing $FILE to $TARGET"
-  install -g $ROOT -o $ROOT -m 0755 $FILE $TARGET
+  cp $FILE $TARGET
 done
